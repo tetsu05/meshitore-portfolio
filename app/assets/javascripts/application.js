5814879c4b8e4e634804105cef8d02b86d1800a6
tetsu05 <tetsu051955@gmail.com>
2021-11-23 12:46:31 +0000

    レイアウト調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,7 @@
 //= require popper
 //= require bootstrap-sprockets
 
-//= require jquery
-//= require jquery_ujs
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   enum subject: [:service,:violation,:other_question]
+  #validates :message, presence: true, lengh: {maximum: 140}
 end
