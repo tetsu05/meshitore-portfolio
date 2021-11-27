87f03097a76e7a5edc29bcba8d30a36f8c503723
tetsu05 <tetsu051955@gmail.com>
2021-11-27 12:51:17 +0000

    バリデーション調整

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ApplicationRecord
   validates :name, presence: true
   validates :message, {presence: true, length: {maximum: 1000}}
   #メールアドレスのバリデーション
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   #電話番号のバリデーション
   validates :phone_number, presence: true, format: {with: /\A[0-9-]{,14}\z/}
 end
